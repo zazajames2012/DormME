6f780a15c99982f214c797e92639c314df7a25ea
Thotsaphront <thotsaphront@ar.co.th>
2018-06-23 18:48:55 +0700

    First

---
+++ DormMEmjuWEB/Controllers/SaveMeterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DormMEmjuWEB.Controllers
+{
+
+    [Authorize]
+    public class SaveMeterController : Controller
+    {
+        // GET: SaveMeter
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
